aa10546260d5ce50a9d67d6f3f7cb8c7d74e4767
Yuki Kurosawa HotKey <yuki@ksyuki.com>
2023-06-06 14:05:16 +0800

    Update Example Program 'test'

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -5,20 +5,41 @@
 TFT_eSPI tft = TFT_eSPI();
 uint16_t bg = ST7735_BLACK;
 uint16_t MenuBackColor = ST7735_BLUE;
-MenuItem Menu[] = {
-  { "Menu 1", {} },
-  { "Menu 2", {} },
-  { "Menu 3", {} },
-  { "Menu 4", {} },
-  { "Menu 5", {} },
-  { "Menu 6", {} },
-  { "Menu 7", {} },
-  { "Menu 8", {} },
-  { "Menu 9", {} },
-  { "Menu 10", {} },
-  { "Menu 11", {} },
-  { "Menu 12", {} }
+uint16_t MenuForeColor = ST7735_WHITE;
+
+MenuItem Menu[] PROGMEM = {
+  { "Menu 1", 3 },
+  { "Menu 2", 0 },
+  { "Menu 3", 0 }  //,
+  // { "Menu 4", 0 },
+  // { "Menu 5", 0 },
+  // { "Menu 6", 0 },
+  // { "Menu 7", 0 },
+  // { "Menu 8", 0 },
+  // { "Menu 9", 0},
+  // { "Menu 10", 0 },
+  // { "Menu 11", 0 },
+  // { "Menu 12", 0 }
 };
+
+MenuItem Menu1[10] = {
+  { "Menu 1.1", 0 },
+  { "Menu 1.2", 4 },
+  { "Menu 1.3", 0 }
+};
+
+MenuItem Menu1_2[10] = {
+  { "Menu 1.2.1", 0 },
+  { "Menu 1.2.2", 0 },
+  { "Menu 1.2.3", 0 },
+  { "Menu 1.2.4", 0 }
+};
+
+
+void bindMenus() {
+  Menu[0].SubMenu = &Menu1;
+  Menu1[1].SubMenu = &Menu1_2;
+}
 
 void setup() {
   tft.begin();
@@ -26,7 +47,10 @@ void setup() {
   tft.setRotation(2);
   tft.loadFont(unifont_en);
   tft.setTextSize(1);
+
+  bindMenus();
   initMenu(Menu, MenuLen(Menu));
+
   Serial.begin(115200);
   while (!Serial) {}
 }
